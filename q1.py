ca0fb0e6ad03c06e223739995cb8a67ecc2bb7d4
JamesALamb <j.a.lamb@hotmail.com>
2018-10-17 13:42:08 +0100

    update q1

--- q1.py
+++ q1.py
@@ -5,27 +5,33 @@ import numpy as np
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
   # modify and then return the variable below
-  mat = np.zeros((len(portfolios), len(portfolios),), dtype=int)
+  #mat = np.zeros((len(portfolios), len(portfolios),), dtype=int)
+  answer = 0
+  index = [0,0]
   for i in range(len(portfolios)):
     for j in range(i+1, len(portfolios)):
       a = np.zeros((16,), dtype=int)
       b = np.zeros((16,), dtype=int)
       p1 = portfolios[i]
       p2 = portfolios[j]
-      for i in range(15,-1,-1):
-        if (p1 - 2**i >= 0):
-          p1 -= 2**i
-          a[i] = 1
-        if (p2 - 2**i >= 0):
-          p2 -= 2**i
-          b[i] = 1
+      for k in range(15,-1,-1):
+        if (p1 - 2**k >= 0):
+          p1 -= 2**k
+          a[15-k] = 1
+        if (p2 - 2**k >= 0):
+          p2 -= 2**k
+          b[15-k] = 1
       c = np.add(a, b)
-      for i in range(len(c)):
-        c[i] %= 2
+      #for k in range(len(c)):
+        #c[k] %= 2
       d = 0
-      for i in range(15,-1,-1):
-        d += c[i] * 2**(15-i)
-      mat[i, j] = d
+      for k in range(16):
+        if (c[k] == 1):
+          d += c[k] * 2**(15-k)
+      if (d > answer):
+        answer = d
+        index = [i,j]
+      #mat[i, j] = d
   #index = np.where(mat == value) 
-  answer = np.max(mat)
+  #answer = np.max(mat)
   return answer
--- q1_4.py
+++ q1_4.py
@@ -6,8 +6,35 @@ import sys
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
   # modify and then return the variable below
-  mat = matrix(portfolios)
-  answer = np.max(mat)
+  #mat = np.zeros((len(portfolios), len(portfolios),), dtype=int)
+  answer = 0
+  index = [0,0]
+  for i in range(len(portfolios)):
+    for j in range(i+1, len(portfolios)):
+      a = np.zeros((16,), dtype=int)
+      b = np.zeros((16,), dtype=int)
+      p1 = portfolios[i]
+      p2 = portfolios[j]
+      for k in range(15,-1,-1):
+        if (p1 - 2**k >= 0):
+          p1 -= 2**k
+          a[15-k] = 1
+        if (p2 - 2**k >= 0):
+          p2 -= 2**k
+          b[15-k] = 1
+      c = np.add(a, b)
+      #for k in range(len(c)):
+        #c[k] %= 2
+      d = 0
+      for k in range(16):
+        if (c[k] == 1):
+          d += c[k] * 2**(15-k)
+      if (d > answer):
+        answer = d
+        index = [i,j]
+      #mat[i, j] = d
+  #index = np.where(mat == value) 
+  #answer = np.max(mat)
   return answer
 
 def max(portfolios):
@@ -25,24 +52,24 @@ def binary(decimal):
   for i in range(15,-1,-1):
     if (decimal - 2**i >= 0):
       decimal -= 2**i
-      binary[i] = 1
+      binary[15-i] = 1
   return binary
 
 # TEST
-# a = int(sys.argv[1])
-# b = binary(a)
-# print b
+a = 5000
+b = binary(a)
+print b
 
 def decimal(binary):
   # get decimal value from binary list
   decimal = 0
-  for i in range(15,-1,-1):
+  for i in range(16):
     decimal += binary[i] * 2**(15-i)
   return decimal
 
 # TEST
-# c = decimal(b)
-# print c
+c = decimal(b)
+print c
 
 def merge(value1, value2):
   # find value of merged portfolios
@@ -75,7 +102,9 @@ portfolios = np.random.randint(65535, size=int(sys.argv[1]))
 
 #print portfolios
 
+answer = question01(portfolios)
 
+print answer
 
 value, index = max(portfolios)
 
