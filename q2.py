166df3913242d88f9c35fc7252edb644ee5fd003
JamesALamb <j.a.lamb@hotmail.com>
2018-10-18 12:29:27 +0100

    done q6

--- q2.py
+++ q2.py
@@ -1,10 +1,21 @@
 # ONLY EDIT FUNCTIONS MARKED CLEARLY FOR EDITING
 
 import numpy as np
+import sys
+
+cashFlowIn = np.random.randint(100, size=int(sys.argv[1]))
+cashFlowOut = np.random.randint(100, size=int(sys.argv[2]))
 
 #compute all combinations for two portfolios
 def question02(cashFlowIn, cashFlowOut):
   # modify and then return the variable below
+  I = np.array(cashFlowIn)
+  O = - np.array(cashFlowOut)
+  combined = np.append(I,O)
+  print combined
+
+
   answer = -1
   return answer
 
+answer = question02(cashFlowIn, cashFlowOut)
--- q6.py
+++ q6.py
@@ -2,8 +2,27 @@
 
 import numpy as np
 
-#compute all combinations for two portfolios
 def question06(numServers, targetServer, times):
   # modify and then return the variable below
-  answer = -1
+  fastest_times = times[0]
+  visited = [0]
+  visit = 0
+  for i in range(numServers):
+    for j in range(1,numServers):
+      if visit in visited:
+        visit = fastest_times.index(sorted(fastest_times)[j])
+      else:
+        break
+    visited.append(visit)
+    #update times
+    new_times = times[visit]
+    for j in range(numServers):
+      if (new_times[j] + fastest_times[visit] < fastest_times[j]):
+        fastest_times[j] = new_times[j] + fastest_times[visit]
+    if (visit == targetServer):
+      break
+
+  answer = fastest_times[targetServer]
+
   return answer
+
