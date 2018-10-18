4237e18ed6d449537961663ad8a8ddf20c4c99bf
JamesALamb <j.a.lamb@hotmail.com>
2018-10-18 13:59:01 +0100

    completed q4

--- q4.py
+++ q4.py
@@ -2,9 +2,38 @@
 
 import numpy as np
 
-#compute all combinations for two portfolios
+X = 'X'
+#rows, numberMachines = [[12, 12, 3, 'X', 3], [23, 'X', 'X', 'X', 3], [33, 21, 'X', 'X', 'X'], [9, 12, 3, 'X', 'X'], ['X', 'X', 'X', 4, 5]] , 3
+#rows, numberMachines = [[X, X, 2], [2, 3, X], [X, 3, 2]], 3
+rows, numberMachines = [[2, 3, X, 2], [4, X, X, 4], [3, 2, X, X], [X, X, X, 5]], 2 
+
 def question04(rows, numberMachines):
   # modify and then return the variable below
-  answer = -1
+  totals = []
+  for row in rows:
+    c = 0
+    values = []
+    for i in row:
+      if i != 'X':
+        values.insert(0, i)
+        c+=1
+        if c >= numberMachines:
+          total=0
+          for j in range(numberMachines):
+            total += values[j]
+          totals.append(total)
+      else:
+        c = 0
+        values = []
+
+  if totals == []:
+    answer = 0
+  else:
+    answer = min(totals)
+
   return answer
+
+a = question04(rows, numberMachines)
+
+print a
 
