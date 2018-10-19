7675c441914e7de06d78b7a2a6599e511baf78a4
JamesALamb <j.a.lamb@hotmail.com>
2018-10-19 14:18:28 +0100

    done q5

--- q4.py
+++ q4.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-#X = 'X'
+X = 'X'
 #rows, numberMachines = [[12, 12, 3, X, 3], [23, X, X, X, 3], [33, 21, X, X, X], [9, 12, 3, X, X], [X, X, X, 4, 5]] , 3
 #rows, numberMachines = [[X, X, 2], [2, 3, X], [X, 3, 2]], 3
 #rows, numberMachines = [[2, 3, X, 2], [4, X, X, 4], [3, 2, X, X], [X, X, X, 5]], 2 
--- q5.py
+++ q5.py
@@ -2,7 +2,24 @@
 
 import numpy as np
 
+#allowedAllocations, totalValue = [1, 3, 4], 6
+#allowedAllocations, totalValue = [3, 4], 6
+
 def question05(allowedAllocations, totalValue):
   # modify and then return the variable below
-  answer = -1
+  allowedAllocations.sort(reverse=True)
+  count = 0
+  value = 0
+  for i in range(len(allowedAllocations)):
+    value += allowedAllocations[i]
+    count +=1
+    if value >= totalValue:
+      break
+
+  answer = count
+
   return answer
+
+#answer = question05(allowedAllocations, totalValue)
+
+#print answer
