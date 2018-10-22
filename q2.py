46374b98d1a660cec837dac9b8d11abc5ef96524
JamesALamb <j.a.lamb@hotmail.com>
2018-10-23 00:09:31 +0100

    done q2

--- q2.py
+++ q2.py
@@ -1,10 +1,13 @@
 # ONLY EDIT FUNCTIONS MARKED CLEARLY FOR EDITING
 
 import numpy as np
-#import sys
 
-#cashFlowIn = np.random.randint(100, size=int(sys.argv[1]))
-#cashFlowOut = np.random.randint(100, size=int(sys.argv[2]))
+#cashflow_in = [66, 293, 215, 188, 147, 326, 449, 162, 46, 350]
+#cashflow_out = [170, 153, 305, 290, 187]
+#cashflow_in = [189, 28]
+#cashflow_out = [43, 267, 112, 166]
+#cashflow_in = [72, 24, 73, 4, 28, 56, 1, 43]
+#cashflow_out = [27]
 
 #compute all combinations for two portfolios
 def question02(cashFlowIn, cashFlowOut):
@@ -12,25 +15,41 @@ def question02(cashFlowIn, cashFlowOut):
   I = np.array(cashFlowIn)
   O = - np.array(cashFlowOut)
   combined = np.append(I,O)
-  A = max(cashFlowIn)
-  B = max(cashFlowOut)
+  A = sum(cashFlowIn)
+  B = sum(cashFlowOut)
   small = min(cashFlowIn)
-  mat = np.full((len(combined), A+B+1), False, dtype=bool)
+  mat = np.full((len(combined)+1, A+B+1), False, dtype=bool)
+  print combined, mat.shape
   for i in range(1,len(mat)):
+    print np.count_nonzero(mat)
+    print mat[i-1,:]
     for j in range(len(mat[0])):
-      if (combined[i-1] == j):
+      if (combined[i-1] == j-B):
+#        print i,j, combined[i-1], j-B
         mat[i,j] = True
+#        print mat[:,j]
       if mat[i-1,j]:
+#        print i, j
         mat[i,j] = True
-      if mat[i-1,j-combined[i-1]]:
-        mat[i,j] = True
+#        print mat[:,j]
+      if ( j-combined[i-1] >= 0 ) and ( j-combined[i-1] < len(mat[0]) ):
+        if mat[i-1,j-combined[i-1]]:
+#        print i, j, combined[i-1], j-combined[i-1]
+          mat[i,j] = True
+#        print mat[:,j]
   for i in range(small):
-    if True in mat[:,B+i]:
+#    print B+i, mat[:,B+i]
+    if mat[len(combined),B+i]:
 
       answer = i
 
       break
+    else:
+
+      answer = small
 
   return answer
 
-#answer = question02(cashFlowIn, cashFlowOut)
+#answer i= question02(cashflow_in, cashflow_out)
+
+#print answer
