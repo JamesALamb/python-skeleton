a2e5bad276ba56a0dccebd68a05ddedb0db60c5c
JamesALamb <j.a.lamb@hotmail.com>
2018-10-22 18:19:14 +0100

    done q2

--- q2.py
+++ q2.py
@@ -12,9 +12,25 @@ def question02(cashFlowIn, cashFlowOut):
   I = np.array(cashFlowIn)
   O = - np.array(cashFlowOut)
   combined = np.append(I,O)
-  
+  A = max(cashFlowIn)
+  B = max(cashFlowOut)
+  small = min(cashFlowIn)
+  mat = np.full((len(combined), A+B+1), False, dtype=bool)
+  for i in range(1,len(mat)):
+    for j in range(len(mat[0])):
+      if (combined[i-1] == j):
+        mat[i,j] = True
+      if mat[i-1,j]:
+        mat[i,j] = True
+      if mat[i-1,j-combined[i-1]]:
+        mat[i,j] = True
+  for i in range(small):
+    if True in mat[:,B+i]:
 
-  answer = -1
+      answer = i
+
+      break
+
   return answer
 
 #answer = question02(cashFlowIn, cashFlowOut)
