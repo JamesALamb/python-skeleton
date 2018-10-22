e88d806580d8d8c62157b8482e79b5972bb8d141
JamesALamb <j.a.lamb@hotmail.com>
2018-10-22 12:42:40 +0100

    q1 updated

--- q1.py
+++ q1.py
@@ -5,6 +5,7 @@ import numpy as np
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
   # modify and then return the variable below
+  print portfolios
   answer = 0
   for i in range(len(portfolios)):
     for j in range(i+1, len(portfolios)):
