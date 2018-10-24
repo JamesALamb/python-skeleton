9cdbd63b66e31a6b0a325446d02b06d073412019
JamesALamb <j.a.lamb@hotmail.com>
2018-10-24 18:02:58 +0100

    update q1

--- q1.py
+++ q1.py
@@ -13,13 +13,32 @@ def question01(portfolios):
     for j in range(i+1, len(portfolios)): 
       a = bin(portfolios[i])[2:].zfill(big)
       b = bin(portfolios[j])[2:].zfill(big)
-      c = merge(a, b) 
-      if (c > answer):
-  
-        answer = c
-  
-  return answer
+      if answer < 2**(big-1):
+        c = merge(a, b)
+        d = c.index('0')
+        c = int(c, 2)
+        if (c > answer):
 
+          answer = c
+
+      else:
+        e = merge(a[:d], b[:d])
+        f = '1'*(d)
+        if e == f:
+          c = merge(a[d:], b[d:])
+          c = f + c
+          if '0' in c:
+            d = c.index('0')
+
+            answer = int(c, 2)
+
+          else:
+
+            answer = int(c, 2)
+
+            return answer
+
+  return answer
 
 
 def merge(a, b):
@@ -31,11 +50,10 @@ def merge(a, b):
   c = [ x % 2 for x in c ]
   c = [ str(x) for x in c ]
   c = ''.join(c)
-  c = int(c, 2)
   return c
 
-#portfolios = [1588038419, 1603264833, 2029937021, 713941009, 923957192, 1118942085, 59239954, 837238868, 753246235, 1403394345, 886500603, 1648810182, 1540165060, 1680351736, 1044516203, 1655352860, 1381992751]
+portfolios = [1588038419, 1603264833, 2029937021, 713941009, 923957192, 1118942085, 59239954, 837238868, 753246235, 1403394345, 886500603, 1648810182, 1540165060, 1680351736, 1044516203, 1655352860, 1381992751]
 
-#answer = question01(portfolios)
+answer = question01(portfolios)
 
-#print answer
+print answer
