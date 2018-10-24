a1a7d876314353d1df6edd1c9eb71471b60e6295
JamesALamb <j.a.lamb@hotmail.com>
2018-10-24 12:10:04 +0100

    update q2

--- q2.py
+++ q2.py
@@ -1,16 +1,11 @@
 # ONLY EDIT FUNCTIONS MARKED CLEARLY FOR EDITING
 
 import numpy as np
-
-#cashflow_in = [66, 293, 215, 188, 147, 326, 449, 162, 46, 350]
-#cashflow_out = [170, 153, 305, 290, 187]
-#cashflow_in = [189, 28]
-#cashflow_out = [43, 267, 112, 166]
-#cashflow_in = [72, 24, 73, 4, 28, 56, 1, 43]
-#cashflow_out = [27]
+import timeit
 
 #compute all combinations for two portfolios
 def question02(cashFlowIn, cashFlowOut):
+  print cashFlowIn, cashFlowOut
   # modify and then return the variable below
   print cashFlowIn, cashFlowOut
   I = np.array(cashFlowIn)
@@ -18,39 +13,28 @@ def question02(cashFlowIn, cashFlowOut):
   combined = np.append(I,O)
   A = sum(cashFlowIn)
   B = sum(cashFlowOut)
-  small = min(cashFlowIn)
   mat = np.full((len(combined)+1, A+B+1), False, dtype=bool)
-#  print combined, mat.shape
   for i in range(1,len(mat)):
-#    print np.count_nonzero(mat)
-#    print mat[i-1,:]
     for j in range(len(mat[0])):
       if (combined[i-1] == j-B):
-#        print i,j, combined[i-1], j-B
         mat[i,j] = True
-#        print mat[:,j]
       if mat[i-1,j]:
-#        print i, j
         mat[i,j] = True
-#        print mat[:,j]
       if ( j-combined[i-1] >= 0 ) and ( j-combined[i-1] < len(mat[0]) ):
         if mat[i-1,j-combined[i-1]]:
-#        print i, j, combined[i-1], j-combined[i-1]
           mat[i,j] = True
-#        print mat[:,j]
-  for i in range(small+1):
-#    print B+i, mat[:,B+i]
-    if mat[len(combined),B+i]:
 
-      answer = i
-
-      break
-    else:
-
-      answer = small
+  answer = mat[len(combined)][B:B+min(cashFlowIn)+1].tolist().index(True)
 
   return answer
 
-#answer i= question02(cashflow_in, cashflow_out)
+#cashFlowIn, cashFlowOut = [79, 954, 372, 842, 982, 562, 740], [513, 398, 535, 223, 693, 206, 922, 877, 486]
+
+#start = timeit.default_timer()
+
+#answer = question02(cashFlowIn, cashFlowOut)
+
+#stop = timeit.default_timer()
 
 #print answer
+#print('Time: ', stop - start)
