3388be91ca1a3fed83d7112535472b8a67534994
JamesALamb <j.a.lamb@hotmail.com>
2018-10-25 12:04:27 +0100

    edit q1

--- q1.py
+++ q1.py
@@ -5,34 +5,57 @@ import numpy as np
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
   # modify and then return the variable below
-  print portfolios
-  answer = 0
+  #print portfolios
   if portfolios == []:
     return 0
   big = len(bin(max(portfolios))[2:])
+  c = merge(bin(portfolios[0])[2:].zfill(big), bin(portfolios[1])[2:].zfill(big))
+  d0 = c.index('0')
+  d1 = c.index('1')
+  index = [0,1]
+
+  answer = int(c, 2)
+
   for i in range(len(portfolios)):
     for j in range(i+1, len(portfolios)): 
       a = bin(portfolios[i])[2:].zfill(big)
       b = bin(portfolios[j])[2:].zfill(big)
-      if answer < 2**(big-1):
-        if a[0] != b[0]:
+      if d0 == 0:
+        if a[:d1] != b[:d1]:
           c = merge(a, b)
-          d = c.index('0')
+          d0 = c.index('0')
+          d1 = c.index('1')
+          #index = [i,j]
 
           answer = int(c, 2)
 
+        else:
+          c = merge(a[d1:], b[d1:]).zfill(big)
+          ans = int(c, 2)
+          if ans > answer:
+            d0 = c.index('0')
+            d1 = c.index('1')
+            #index = [i,j]
+
+            answer = ans
+
       else:
-        e = merge(a[:d], b[:d])
-        f = '1'*(d)
+        e = merge(a[:d0], b[:d0])
+        f = '1'*(d0)
         if e == f:
-          c = merge(a[e:], b[e:])
+          c = merge(a[d0:], b[d0:])
           c = f + c
           if '0' in c:
-            d = c.index('0')
+            ans = int(c, 2)
+            if ans > answer:
+              d0 = c.index('0')
+              d1 = c.index('1')
+              #index = [i,j]
 
-            answer = int(c, 2)
+              answer = ans
 
           else:
+            #index = [i,j]
 
             answer = int(c, 2)
 
