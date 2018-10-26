153bc4a7c19e84fcef06a71b9e9fe430d9ca92ba
JamesALamb <j.a.lamb@hotmail.com>
2018-10-26 14:10:59 +0100

    final q123456

--- q1.py
+++ q1.py
@@ -5,56 +5,54 @@ import numpy as np
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
   # modify and then return the variable below
-  if portfolios == []:
+  if len(portfolios) < 2:
     return 0
   big = len(bin(max(portfolios))[2:])
-  c = merge(bin(portfolios[0])[2:].zfill(big), bin(portfolios[1])[2:].zfill(big))
-  d0 = c.index('0')
-  d1 = c.index('1')
-  index = [0,1]
-
-  answer = int(c, 2)
+  answer = 0
+  d0 = 0
+  d1 = big+1
 
   for i in range(len(portfolios)):
     for j in range(i+1, len(portfolios)): 
-      a = bin(portfolios[i])[2:].zfill(big)
-      b = bin(portfolios[j])[2:].zfill(big)
-      if d0 == 0:
-        if a[:d1] != b[:d1]:
-          c = merge(a, b)
-          d0 = c.index('0')
-          d1 = c.index('1')
-
-          answer = int(c, 2)
-
-        else:
-          c = merge(a[d1:], b[d1:]).zfill(big)
-          ans = int(c, 2)
-          if ans > answer:
+      if portfolios[i] != portfolios[j]:
+        a = bin(portfolios[i])[2:].zfill(big)
+        b = bin(portfolios[j])[2:].zfill(big)
+        if d0 == 0:
+          if a[:d1] != b[:d1]:
+            c = merge(a, b)
             d0 = c.index('0')
             d1 = c.index('1')
-
-            answer = ans
-
-      else:
-        e = merge(a[:d0], b[:d0])
-        f = '1'*(d0)
-        if e == f:
-          c = merge(a[d0:], b[d0:])
-          c = f + c
-          if '0' in c:
+  
+            answer = int(c, 2)
+  
+          else:
+            c = merge(a[d1:], b[d1:]).zfill(big)
             ans = int(c, 2)
             if ans > answer:
               d0 = c.index('0')
               d1 = c.index('1')
-
+  
               answer = ans
-
-          else:
-
-            answer = int(c, 2)
-
-            return answer
+  
+        else:
+          e = merge(a[:d0], b[:d0])
+          f = '1'*(d0)
+          if e == f:
+            c = merge(a[d0:], b[d0:])
+            c = f + c
+            if '0' in c:
+              ans = int(c, 2)
+              if ans > answer:
+                d0 = c.index('0')
+                d1 = c.index('1')
+  
+                answer = ans
+  
+            else:
+  
+              answer = int(c, 2)
+  
+              return answer
 
   return answer
 
